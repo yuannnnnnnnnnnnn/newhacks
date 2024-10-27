6802dda55b5f1ed52f3eafce4b197772b2dd63e6
lan-t-huynh <l.h.lanhuynh@gmail.com>
2024-10-27 01:13:27 -0400

    coin count added

--- logos/game_ver3.py
+++ logos/game_ver3.py
@@ -7,12 +7,21 @@ HEIGHT = 400
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Do Not Leave Mike Naked')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font(None, 50)
+test_font = pygame.font.Font(None, 36)
+
+coins = 0
 
 background_surface = pygame.image.load('background.jpg').convert()
 background_surface = pygame.transform.scale(background_surface, (WIDTH, HEIGHT))
 
-text_surface = test_font.render('My game', True, 'Green')
+score_surf = test_font.render(str(coins), True, 'White')
+score_rect = score_surf.get_rect(topright = (545, 14))
+
+hearts_surf = 
+
+coin_surf = pygame.image.load('coin.png').convert_alpha()
+coin_surf = pygame.transform.scale(coin_surf, (50, 30))
+coin_rect = coin_surf.get_rect(topright = (590,10))
 
 rock_surface = pygame.image.load('better_rock.png').convert_alpha()
 rock_surface = pygame.transform.scale(rock_surface, (70, 35))
@@ -27,13 +36,23 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rect.collidepoint(event.pos): print('collision')
     
     screen.blit(background_surface, (0,0))
-    screen.blit(text_surface, (230, 50))
+    screen.blit(score_surf, score_rect)
+    screen.blit(coin_surf, coin_rect)
     rock_rect.x -= 4
     if rock_rect.right <= 0: rock_rect.left = 600
     screen.blit(rock_surface, rock_rect)
     screen.blit(player_surf, player_rect)
+
+    # if player_rect.colliderect(snail_rect):
+    #     print('collision')
+
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint(mouse_pos):
+    #     print(pygame.mouse.get_pressed())
 
     pygame.display.update()
     clock.tick(60)

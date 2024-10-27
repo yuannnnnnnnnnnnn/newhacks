aa53e06c0ca10f961ac18b9116fbcd167b6e55d5
lan-t-huynh <l.h.lanhuynh@gmail.com>
2024-10-26 23:02:14 -0400

    dfg

--- logos/game_ver3.py
+++ logos/game_ver3.py
@@ -6,12 +6,15 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Do Not Leave Mike Naked')
 clock = pygame.time.Clock()
 
+test_surface = pygame.Surface((100,200))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    #draw all our elements
-    #update everything
+    
+    screen.blit(test_surface, (0,0))
+
     pygame.display.update()
     clock.tick(60)

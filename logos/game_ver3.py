9d470f51ef72c524f06e87370a1beca8904ddd9c
lan-t-huynh <l.h.lanhuynh@gmail.com>
2024-10-26 23:22:31 -0400

    ghjk

--- logos/game_ver3.py
+++ logos/game_ver3.py
@@ -2,11 +2,17 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((600,400))
+WIDTH = 600
+HEIGHT = 400
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Do Not Leave Mike Naked')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font(None, 50)
 
 background_surface = pygame.image.load('background.jpg')
+text_surface = test_font.render('My game', True, 'Green')
+
+rock_surface = pygame.image.load('rock.png')
 
 while True:
     for event in pygame.event.get():
@@ -15,6 +21,8 @@ while True:
             exit()
     
     screen.blit(background_surface, (0,0))
+    screen.blit(text_surface, (230, 50))
+    screen.blit(rock_surface, (0,0))
 
     pygame.display.update()
     clock.tick(60)
